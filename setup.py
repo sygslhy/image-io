17e794d826b66ad2b3edd59d471463663b2fd424
Yuan SUN <ysun@impactengineering.fr>
2023-12-13 11:14:06 +0100

    version 0.0.6 just to correct the typo in readme

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="cxx-image-io",
-    version="0.0.5",
+    version="0.0.6",
     author="Yuan SUN",
     author_email="sunyuan860510@gmail.com",
     description="Python image IO module with binding cxx image code",
