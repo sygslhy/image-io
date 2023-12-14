2a2dd6e7b4189f7bea9b765bf85140331669ee9c
Yuan SUN <ysun@impactengineering.fr>
2023-12-14 10:18:24 +0100

    remove the test part to an indepentant repo

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-# Append directly the .pyd file in build directory to sys.path
-# So we can run the test without installation package.
-current_dir = Path(__file__).parent.parent
-image_io_lib_path = Path(current_dir, 'build', 'cxx_image_io', 'binding')
-sys.path.append(str(image_io_lib_path))
