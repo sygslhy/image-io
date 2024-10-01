bead297909d25ce916542e53ed715f2b320c4193
Yuan SUN <sunyuan860510@gmail.com>
2024-10-01 09:28:39 +0200

    add extra catch in python level to have readable format on C++ exception infos

--- cxx_image_io/io.py
+++ cxx_image_io/io.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import logging
 from cxx_image import (ExifMetadata, ImageDouble, ImageFloat, ImageInt, ImageLayout,
                        ImageMetadata, ImageUint8, ImageUint16,
                        PixelRepresentation, io, parser)
@@ -11,6 +13,11 @@ __numpy_array_image_convert_vector = {
     np.dtype('int'): ImageInt
 }
 
+# internal function to print image essentiel infos for debugging
+def __print_image_metadata_info(metadata):
+    print('Image Type:', metadata.fileInfo.pixelType)
+    print('Image Precision:', metadata.fileInfo.pixelPrecision)
+    print('Image Layout:', metadata.fileInfo.imageLayout)
 
 # fill the image critical information to metadata that could be used otherwhere.
 def __fill_medatata(image, metadata):
@@ -38,31 +45,33 @@ def read_image(image_path: str, metadata_path: str = None) -> np.array:
         returned image in numpy array format
 
     """
+    try:
+        metadata = parser.readMetadata(image_path, metadata_path)
 
-    metadata = parser.readMetadata(image_path, metadata_path)
+        #[Fix Issue 1] Currently pybind11 buffer protocol support only memory mapping of the image having
+        # the same size on each planars, so the different sizes on each planars like nv12, yuv are not yet support
+        if metadata and metadata.fileInfo.imageLayout in (ImageLayout.YUV_420, ImageLayout.NV12):
+            raise Exception("Cannot support convert the different sizes planes image, like nv12 and yuv to numpy array")
 
-    #[Fix Issue 1] Currently pybind11 buffer protocol support only memory mapping of the image having
-    # the same size on each planars, so the different sizes on each planars like nv12, yuv are not yet support
-    if metadata:
-        assert metadata.fileInfo.imageLayout not in (ImageLayout.YUV_420, ImageLayout.NV12
-            ), "Cannot support convert the different sizes planes image, like nv12 and yuv to numpy array"
-
-    image_reader = io.makeReader(image_path, metadata)
-    # Currently don't find a good way to supported completely the std::optional by binding C++ function.
-    # So create explicitily an ImageMetadata object when metadata is None.
-    metadata = ImageMetadata() if metadata is None else metadata
-    metadata = image_reader.readMetadata(metadata)
-    if image_reader.pixelRepresentation() == PixelRepresentation.UINT8:
-        image = image_reader.read8u()
-    elif image_reader.pixelRepresentation() == PixelRepresentation.UINT16:
-        image = image_reader.read16u()
-    elif image_reader.pixelRepresentation() == PixelRepresentation.FLOAT:
-        image = image_reader.readf()
-    else:
-        raise Exception('Unsupported image type!')
-    metadata = __fill_medatata(image, metadata)
-    return np.array(image, copy=False), metadata
-
+        image_reader = io.makeReader(image_path, metadata)
+        # Currently don't find a good way to supported completely the std::optional by binding C++ function.
+        # So create explicitily an ImageMetadata object when metadata is None.
+        metadata = ImageMetadata() if metadata is None else metadata
+        metadata = image_reader.readMetadata(metadata)
+        if image_reader.pixelRepresentation() == PixelRepresentation.UINT8:
+            image = image_reader.read8u()
+        elif image_reader.pixelRepresentation() == PixelRepresentation.UINT16:
+            image = image_reader.read16u()
+        elif image_reader.pixelRepresentation() == PixelRepresentation.FLOAT:
+            image = image_reader.readf()
+        else:
+            raise Exception('Unsupported image type!')
+        metadata = __fill_medatata(image, metadata)
+        return np.array(image, copy=False), metadata
+    except Exception as e:
+        logging.error('Exception occurred in reading image from file {0}: {1}'.format(image_path, e))
+        __print_image_metadata_info(metadata)
+        sys.exit(-1)
 
 def read_exif(image_path: str) -> ExifMetadata:
     """Read the exif data from image
@@ -77,12 +86,16 @@ def read_exif(image_path: str) -> ExifMetadata:
     ExifMetadata
         returned exif data
     """
-    # By binding parser.readMetadata C++ code, we need to privode explicitely None as metadata path
-    # In the case of tif, jpg or dng, we don't need sidecar.
-    metadata = parser.readMetadata(image_path, None)
-    image_reader = io.makeReader(image_path, metadata)
-    return image_reader.readExif()
-
+    try:
+        # By binding parser.readMetadata C++ code, we need to privode explicitely None as metadata path
+        # In the case of tif, jpg or dng, we don't need sidecar.
+        metadata = parser.readMetadata(image_path, None)
+        image_reader = io.makeReader(image_path, metadata)
+        return image_reader.readExif()
+    except Exception as e:
+        logging.error('Exception occurred in reading exif from file {0}: {1}'.format(image_path, e))
+        __print_image_metadata_info(metadata)
+        sys.exit(-1)
 
 def write_image(output_path: str, image_array: np.array,
                 write_options: io.ImageWriter.Options):
@@ -98,23 +111,28 @@ def write_image(output_path: str, image_array: np.array,
     write_options : io.ImageWriter.Options
         write options for writing parameters like, image buffer info, jpegQualiy, tiff compression type and exif metadata infos. by default None
     """
+    try:
+        # Currently don't find a good way to supported completely the std::optional by binding C++ function.
+        # So create explicitily an ImageWriter.Options object when write_options is None.
+        options = io.ImageWriter.Options(
+        ) if write_options is None else write_options
 
-    # Currently don't find a good way to supported completely the std::optional by binding C++ function.
-    # So create explicitily an ImageWriter.Options object when write_options is None.
-    options = io.ImageWriter.Options(
-    ) if write_options is None else write_options
+        # Before writing the numpy array to image file, it is necessary to convert numpy array to C++ image object.
+        # However, only numpy array need to know image types-map from np.dtype to Image<T>,
+        # so we need firstly __numpy_array_image_convert_vector here
+        # In addition, numpy array can't provide enough information to create C++ image object from itself.
+        # We still need to get the information from metadata.fileInfo in write_options.
+        image = __numpy_array_image_convert_vector[image_array.dtype](
+            image_array, options.metadata.fileInfo.pixelType,
+            options.metadata.fileInfo.imageLayout,
+            options.metadata.fileInfo.pixelPrecision)
 
-    # Before writing the numpy array to image file, it is necessary to convert numpy array to C++ image object.
-    # However, only numpy array need to know image types-map from np.dtype to Image<T>,
-    # so we need firstly __numpy_array_image_convert_vector here
-    # In addition, numpy array can't provide enough information to create C++ image object from itself.
-    # We still need to get the information from metadata.fileInfo in write_options.
-    image = __numpy_array_image_convert_vector[image_array.dtype](
-        image_array, options.metadata.fileInfo.pixelType,
-        options.metadata.fileInfo.imageLayout,
-        options.metadata.fileInfo.pixelPrecision)
-    image_writer = io.makeWriter(output_path, options)
-    image_writer.write(image)
+        image_writer = io.makeWriter(output_path, options)
+        image_writer.write(image)
+    except Exception as e:
+        logging.error('Exception occurred in writing image to file {0}: {1}'.format(output_path, e))
+        __print_image_metadata_info(options.metadata)
+        sys.exit(-1)
 
 
 def write_exif(image_path: str, exif: ExifMetadata = None):
@@ -127,5 +145,9 @@ def write_exif(image_path: str, exif: ExifMetadata = None):
     exif : ExifMetadata
         exif data to write
     """
-    image_writer = io.makeWriter(image_path, io.ImageWriter.Options())
-    image_writer.writeExif(exif)
+    try:
+        image_writer = io.makeWriter(image_path, io.ImageWriter.Options())
+        image_writer.writeExif(exif)
+    except Exception as e:
+        logging.error('Exception occurred in writing exif to file {0}: {1}'.format(image_path, e))
+        sys.exit(-1)
