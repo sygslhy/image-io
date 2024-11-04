9856f1b71aa899cd915c4f03485a8643e0dac416
Yuan SUN <sunyuan860510@gmail.com>
2024-11-04 14:04:42 +0100

    [Release]add test pypi step in workflow

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="cxx-image-io",
-    version="0.0.18",
+    version="0.0.19",
     author="Yuan SUN",
     author_email="sunyuan860510@gmail.com",
     description="Python image IO module with binding cxx image code",
