51668bac7ad382cce550c28654df5faf58cfb59f
Yuan SUN <sunyuan860510@gmail.com>
2025-02-03 23:47:03 +0100

    create the first binding mode of libraw-type

--- binding/cxx_libraw/BindingEntryPoint.cpp
+++ binding/cxx_libraw/BindingEntryPoint.cpp
@@ -1,19 +1,15 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
-#include "loguru.hpp"
-
 namespace py = pybind11;
+
+void init_types(py::module &);
 
 PYBIND11_MODULE(cxx_libraw, m) {
     m.doc() = "libraw binding module";
 
-    // Logoru issue: need to set Verbosity_OFF explicitly at binding call,
-    // because loguru default value is Verbosity_OFF, it suppprts only configure
-    // it by command line. since binding call is thourgh program directly, not
-    // through command line, so this setting is necessary.
-    loguru::g_stderr_verbosity = loguru::Verbosity_WARNING;
-
     // initialize all the binding submodules.
+    init_types(m);
+
 
 }
